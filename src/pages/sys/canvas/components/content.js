8a6e0204c96eec2930b0ddbb1cba825358e307e4
hanyueqiang <webyueqiang@163.com>
2019-09-02 23:24:52 +0800

    canvas

--- src/pages/sys/canvas/components/content.js
+++ src/pages/sys/canvas/components/content.js
@@ -20,6 +20,9 @@ class Content extends Component {
     });
 
   }
+  lightHandle = (item) => {
+    console.log(item)
+  }
 
   render() {
     const { canvasContent, defectsSource } = this.props;
@@ -29,22 +32,21 @@ class Content extends Component {
       <div className={styles.canvas_content}>
         <canvas className="myCanvas" style={{ position: 'absolute', zIndex: 999, left: 0, right: 0, top: 0, bottom: 0, pointerEvents: 'none' }}></canvas>
         <Row gutter={20}>
-          <Col md={24} lg={24} xl={17} >
+          <Col md={17} lg={17} xl={17} >
             <Card
               title={'副主任医师查房记录'}
               style={{ marginTop: 20, height: 510 }}
             >
-              <p>主任医师查房记录</p>
-
-              asd
+              <div style={{fontSize: 14, textAlign:'center',marginBottom: 6}}>{canvasContent.info}</div>
+              <div style={{fontSize: 14,lineHeight:'22px'}}>{canvasContent.content}</div>
           </Card>
           </Col>
-          <Col md={24} lg={24} xl={7} >
+          <Col md={7} lg={7} xl={7} >
             <Card
               title={'缺陷列表'}
               style={{ marginTop: 20, height: 510, overflow: 'hidden' }}
             >
-              qwer
+              {defectsSource.map(item => <div onClick={this.lightHandle.bind(this, item)} className={styles.rightCard} key={item.id}>{item.name}</div>)}
           </Card>
           </Col>
         </Row>
